e14905ee2597212582f761c11825354af100090b
Brondold <114911762+Brondold@users.noreply.github.com>
2023-01-27 01:59:23 +0100

    Modification : Map | Adding Winning Sound

--- VampireSurvivors/Assets/Scripts/GameWinPanel.cs
+++ VampireSurvivors/Assets/Scripts/GameWinPanel.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class GameWinPanel : MonoBehaviour
 {
+
+    public AudioSource audioSource;
+    public AudioClip clip;
+    public float volume = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource.PlayOneShot(clip, volume);
     }
 
     // Update is called once per frame
